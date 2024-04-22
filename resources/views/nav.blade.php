9404d38d1b4917de293494317f75220cf03c8769
Orlandaisekouadio <jkouadio759@gmail.com>
2024-04-22 16:45:38 +0000

    Vue principale et model Task

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-expand navbar-dark bg-dark fixed-top">
+    <div class="container-fluid">
+        <a href="#" class="navbar-brand">
+            <span class="badge bg-success shadow">TODOLIST</span>
+        </a>
+        <ul class="navbar-nav ">
+            <li class="nav-item "><a href="" class="nav-link active">Toutes</a></li>
+            <li class="nav-item "><a href="" class="nav-link ">En cours</a></li>
+            <li class="nav-item "><a href="" class="nav-link ">Terminées</a></li>
+        </ul>
+    </div>
+</nav>
---
+++ resources/views/task/index.blade.php
@@ -0,0 +1,201 @@
+@extends('app')
+
+@section('content')
+    <div class="container-fluid mt-5">
+        <h2 class="text-success text-center "> Tâches en cours </h2>
+        <div class="row justify-content-center">
+            <div class="col-5 col-md-4">
+                <div class="card mt-3 shadow pull_up ">
+                    <div class="card-body">
+                        <div class="card-tite h5 text-secondary">
+                            Titre
+                        </div>
+                        <div class="card-text text-black">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat saepe minus blanditiis sunt
+                            odio. Minima aliquid tempora sequi et quisquam!
+                        </div>
+                        <div class="btn-group mt-3">
+                            <button class="btn btn-success">
+                                Terminer
+                            </button>
+                            <button class="btn btn-danger">
+                                Supprimer
+                            </button>
+                            <button class="btn btn-primary">
+                                Modifier
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 col-md-4">
+                <div class="card mt-3 shadow pull_up ">
+                    <div class="card-body">
+                        <div class="card-tite h5 text-secondary">
+                            Titre
+                        </div>
+                        <div class="card-text text-black">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat saepe minus blanditiis sunt
+                            odio. Minima aliquid tempora sequi et quisquam!
+                        </div>
+                        <div class="btn-group mt-3">
+                            <button class="btn btn-success">
+                                Terminer
+                            </button>
+                            <button class="btn btn-danger">
+                                Supprimer
+                            </button>
+                            <button class="btn btn-primary">
+                                Modifier
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 col-md-4">
+                <div class="card mt-3 shadow pull_up ">
+                    <div class="card-body">
+                        <div class="card-tite h5 text-secondary">
+                            Titre
+                        </div>
+                        <div class="card-text text-black">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat saepe minus blanditiis sunt
+                            odio. Minima aliquid tempora sequi et quisquam!
+                        </div>
+                        <div class="btn-group mt-3">
+                            <button class="btn btn-success">
+                                Terminer
+                            </button>
+                            <button class="btn btn-danger">
+                                Supprimer
+                            </button>
+                            <button class="btn btn-primary">
+                                Modifier
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 col-md-4">
+                <div class="card mt-3 shadow pull_up ">
+                    <div class="card-body">
+                        <div class="card-tite h5 text-secondary">
+                            Titre
+                        </div>
+                        <div class="card-text text-black">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat saepe minus blanditiis sunt
+                            odio. Minima aliquid tempora sequi et quisquam!
+                        </div>
+                        <div class="btn-group mt-3">
+                            <button class="btn btn-success">
+                                Terminer
+                            </button>
+                            <button class="btn btn-danger">
+                                Supprimer
+                            </button>
+                            <button class="btn btn-primary">
+                                Modifier
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 col-md-4">
+                <div class="card mt-3 shadow pull_up ">
+                    <div class="card-body">
+                        <div class="card-tite h5 text-secondary">
+                            Titre
+                        </div>
+                        <div class="card-text text-black">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat saepe minus blanditiis sunt
+                            odio. Minima aliquid tempora sequi et quisquam!
+                        </div>
+                        <div class="btn-group mt-3">
+                            <button class="btn btn-success">
+                                Terminer
+                            </button>
+                            <button class="btn btn-danger">
+                                Supprimer
+                            </button>
+                            <button class="btn btn-primary">
+                                Modifier
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 col-md-4">
+                <div class="card mt-3 shadow pull_up ">
+                    <div class="card-body">
+                        <div class="card-tite h5 text-secondary">
+                            Titre
+                        </div>
+                        <div class="card-text text-black">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat saepe minus blanditiis sunt
+                            odio. Minima aliquid tempora sequi et quisquam!
+                        </div>
+                        <div class="btn-group mt-3">
+                            <button class="btn btn-success">
+                                Terminer
+                            </button>
+                            <button class="btn btn-danger">
+                                Supprimer
+                            </button>
+                            <button class="btn btn-primary">
+                                Modifier
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 col-md-4">
+                <div class="card mt-3 shadow pull_up ">
+                    <div class="card-body">
+                        <div class="card-tite h5 text-secondary">
+                            Titre
+                        </div>
+                        <div class="card-text text-black">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat saepe minus blanditiis sunt
+                            odio. Minima aliquid tempora sequi et quisquam!
+                        </div>
+                        <div class="btn-group mt-3">
+                            <button class="btn btn-success">
+                                Terminer
+                            </button>
+                            <button class="btn btn-danger">
+                                Supprimer
+                            </button>
+                            <button class="btn btn-primary">
+                                Modifier
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 col-md-4">
+                <div class="card mt-3 shadow pull_up ">
+                    <div class="card-body">
+                        <div class="card-tite h5 text-secondary">
+                            Titre
+                        </div>
+                        <div class="card-text text-black">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat saepe minus blanditiis sunt
+                            odio. Minima aliquid tempora sequi et quisquam!
+                        </div>
+                        <div class="btn-group mt-3">
+                            <button class="btn btn-success">
+                                Terminer
+                            </button>
+                            <button class="btn btn-danger">
+                                Supprimer
+                            </button>
+                            <button class="btn btn-primary">
+                                Modifier
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
