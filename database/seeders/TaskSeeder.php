408af7c6f6259ec6e87e34ad1dfd135b2ccc0cb2
Yann Moïse <mkyann2021@gmail.com>
2024-04-23 10:02:50 +0000

    Etablissement du TaskSeeder

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use app\Models\ Task;
 class TaskSeeder extends Seeder
 {
     /**
@@ -12,6 +12,8 @@ class TaskSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //Utilisation de la TaskFactory
+
+        Task::factory()->count(6)->create();
     }
 }
